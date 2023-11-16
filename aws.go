a2572d9f6cbdc4df5f3b6c338632f72d7af6a002
Adalberto Junior <adalberto.junior@baseservice.io>
2023-11-16 12:57:40 -0300

    errors

--- aws.go
+++ aws.go
@@ -23,13 +23,13 @@ type ProfileRegion struct {
 }
 
 type Aws struct {
-	awsCfg    aws.Config
-	pr        ProfileRegion
-	r53Client *route53.Client
-	s3Client  *s3.Client
-	iamClient *iam.Client
-	ecrClient *ecr.Client
-	ssmClient *ssm.Client
+	AwsCfg    aws.Config
+	Pr        ProfileRegion
+	R53Client *route53.Client
+	S3Client  *s3.Client
+	IamClient *iam.Client
+	EcrClient *ecr.Client
+	SsmClient *ssm.Client
 }
 
 type AwsBuilder struct {
@@ -37,13 +37,13 @@ type AwsBuilder struct {
 }
 
 func (b *AwsBuilder) MustAWSConfig(profile, region string) *AwsBuilder {
-	b.pr.profile = profile
-	b.pr.region = region
+	b.Pr.profile = profile
+	b.Pr.region = region
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithSharedConfigProfile(profile), config.WithDefaultRegion(region))
 	if err != nil {
 		panic(err)
 	}
-	b.awsCfg = cfg
+	b.AwsCfg = cfg
 	return b
 }
 
@@ -54,42 +54,42 @@ func NewAwsBuilder() *AwsBuilder {
 }
 
 func (b *AwsBuilder) WithR53() *AwsBuilder {
-	r53 := route53.NewFromConfig(b.awsCfg)
-	b.r53Client = r53
+	r53 := route53.NewFromConfig(b.AwsCfg)
+	b.R53Client = r53
 	return b
 }
 
 func (b *AwsBuilder) WithS3() *AwsBuilder {
-	s3 := s3.NewFromConfig(b.awsCfg)
-	b.s3Client = s3
+	s3 := s3.NewFromConfig(b.AwsCfg)
+	b.S3Client = s3
 	return b
 }
 
 func (b *AwsBuilder) WithIam() *AwsBuilder {
-	iam := iam.NewFromConfig(b.awsCfg)
-	b.iamClient = iam
+	iam := iam.NewFromConfig(b.AwsCfg)
+	b.IamClient = iam
 	return b
 }
 
 func (b *AwsBuilder) WithEcr() *AwsBuilder {
-	ecr := ecr.NewFromConfig(b.awsCfg)
-	b.ecrClient = ecr
+	ecr := ecr.NewFromConfig(b.AwsCfg)
+	b.EcrClient = ecr
 	return b
 }
 
 func (b *AwsBuilder) WithSSM() *AwsBuilder {
-	ssm := ssm.NewFromConfig(b.awsCfg)
-	b.ssmClient = ssm
+	ssm := ssm.NewFromConfig(b.AwsCfg)
+	b.SsmClient = ssm
 	return b
 }
 
 func (b *AwsBuilder) Build() (*Aws, error) {
 
-	if b.pr.profile == "" {
+	if b.Pr.profile == "" {
 		return nil, ErrNoProfile
 	}
 
-	if b.pr.region == "" {
+	if b.Pr.region == "" {
 		return nil, ErrNoRegion
 	}
 
